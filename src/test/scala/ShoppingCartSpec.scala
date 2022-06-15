c52e8c43d84e916c16db938128affd0e5fba71f9
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2022-06-15 18:05:09 +0100

    Step 2 introduced new tests.

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -8,4 +8,18 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
     ShoppingCart.total(Seq(Apple)) shouldEqual Apple.price
     ShoppingCart.total(Seq(Orange)) shouldEqual Orange.price
   }
+
+  "Shopping Cart" should "apply discount on: buy one, get one free on Apples" in {
+    ShoppingCart.total(Seq(Apple)) shouldEqual Apple.price
+    ShoppingCart.total(Seq(Apple, Apple)) shouldEqual Apple.price
+    ShoppingCart.total(Seq(Apple, Apple, Apple)) shouldEqual 2 * Apple.price
+    ShoppingCart.total(Seq(Apple, Apple, Apple, Apple)) shouldEqual 3 * Apple.price
+  }
+
+  "Shopping Cart" should "apply discount on: 3 for the price of 2 on Oranges" in {
+    ShoppingCart.total(Seq(Orange, Orange)) shouldEqual 2 * Orange.price
+    ShoppingCart.total(Seq(Orange,Orange,Orange)) shouldEqual 2 * Orange.price
+    ShoppingCart.total(Seq(Orange, Orange, Orange, Orange)) shouldEqual 3 * Orange.price
+    ShoppingCart.total(Seq(Orange, Orange, Orange, Orange, Orange, Orange, Orange)) shouldEqual 5 * Orange.price
+  }
 }
