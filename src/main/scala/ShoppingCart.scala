9a7575ed3dbbe632b6ab82b86339921b23cbeea9
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2022-06-15 17:57:57 +0100

    Step 1 Implemented, fixed tests.

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,3 +1,16 @@
 
+trait Product {
+    def price:Double
+}
+
+object Apple extends Product {
+    def price = 0.60
+}
+
+object Orange extends Product {
+    def price = 0.25
+}
+
 object ShoppingCart {
+    def total(products:Seq[Product]) = products.map(_.price).sum
 }
