37d4f342f91ce3384c5b147939773d11bfb91bad
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2022-06-15 17:59:49 +0100

    Test price values are no longer hard-coded.

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -4,8 +4,8 @@ import org.scalatest.matchers.should.Matchers
 class ShoppingCartSpec extends AnyFlatSpec with Matchers {
   "Shopping Cart" should "output total price of items" in {
     ShoppingCart.total(Seq()) shouldEqual 0
-    ShoppingCart.total(Seq(Apple, Apple, Orange, Apple)) shouldEqual 2.05
-    ShoppingCart.total(Seq(Apple)) shouldEqual 0.60
-    ShoppingCart.total(Seq(Orange)) shouldEqual 0.25
+    ShoppingCart.total(Seq(Apple, Apple, Orange, Apple)) shouldEqual (Apple.price * 3 + Orange.price)
+    ShoppingCart.total(Seq(Apple)) shouldEqual Apple.price
+    ShoppingCart.total(Seq(Orange)) shouldEqual Orange.price
   }
 }
