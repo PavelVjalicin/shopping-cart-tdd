52eff27dc251d67baeb1ac970b772bf8bafec804
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2022-06-15 18:20:31 +0100

    Step 2 Refactored tests for better readability. Added additional test for a potential edge-case.

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -2,12 +2,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ShoppingCartSpec extends AnyFlatSpec with Matchers {
-  "Shopping Cart" should "output total price of items" in {
-    ShoppingCart.total(Seq()) shouldEqual 0
-    ShoppingCart.total(Seq(Apple, Orange)) shouldEqual (Apple.price + Orange.price)
-    ShoppingCart.total(Seq(Apple)) shouldEqual Apple.price
-    ShoppingCart.total(Seq(Orange)) shouldEqual Orange.price
-  }
 
   "Shopping Cart" should "apply discount on: buy one, get one free on Apples" in {
     ShoppingCart.total(Seq(Apple)) shouldEqual Apple.price
@@ -21,5 +15,13 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
     ShoppingCart.total(Seq(Orange,Orange,Orange)) shouldEqual 2 * Orange.price
     ShoppingCart.total(Seq(Orange, Orange, Orange, Orange)) shouldEqual 3 * Orange.price
     ShoppingCart.total(Seq(Orange, Orange, Orange, Orange, Orange, Orange, Orange)) shouldEqual 5 * Orange.price
+  }
+
+  "Shopping Cart" should "output total price of items with discounts" in {
+    ShoppingCart.total(Seq()) shouldEqual 0
+    ShoppingCart.total(Seq(Apple, Orange)) shouldEqual (Apple.price + Orange.price)
+    ShoppingCart.total(Seq(Apple)) shouldEqual Apple.price
+    ShoppingCart.total(Seq(Orange)) shouldEqual Orange.price
+    ShoppingCart.total(Seq(Apple, Orange, Orange, Orange, Apple, Apple)) shouldEqual 2 * Apple.price + 2 * Orange.price
   }
 }
